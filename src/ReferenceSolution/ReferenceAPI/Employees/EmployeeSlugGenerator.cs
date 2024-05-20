524a6d59b2a1ef811d7fae48a688f80a1efddfc1
Dana Marcusanu <dmarcusanu@gmail.com>
2024-05-20 15:12:18 -0400

    drm

---
+++ src/ReferenceSolution/ReferenceAPI/Employees/EmployeeSlugGenerator.cs
@@ -0,0 +1,12 @@
+﻿
+namespace ReferenceAPI.Employees;
+
+public class EmployeeSlugGenerator
+{
+    public string Generate(string firstName, string lastName)
+    {
+        if (string.IsNullOrEmpty(lastName))
+            return firstName.ToLowerInvariant();
+        return $"{lastName.ToLowerInvariant()}-{firstName.ToLowerInvariant()}";
+    }
+}
---
+++ src/ReferenceSolution/ReferenceApi.UnitTests/SlugGeneratorTests.cs
@@ -0,0 +1,20 @@
+﻿using ReferenceAPI.Employees;
+
+namespace ReferenceApi.UnitTests;
+public class SlugGeneratorTests
+{
+    [Theory]
+    [InlineData("Boba", "Fett", "fett-boba")]
+    [InlineData("Luke", "Skywalker", "skywalker-luke")]
+    [InlineData("Joe", "", "joe")]
+    [InlineData("Cher", "", "cher")]
+    public void Avocado(string firstName, string lastName, string expected)
+    {
+        //Write the code you wish you had
+        var slugGenerator = new EmployeeSlugGenerator();
+
+        string slug = slugGenerator.Generate(firstName, lastName);
+
+        Assert.Equal(expected, slug);
+    }
+}
