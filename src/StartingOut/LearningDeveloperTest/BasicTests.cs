ada9f909dde52c713d7413c46c97035453a19f8e
Dana Marcusanu <dmarcusanu@gmail.com>
2024-05-20 11:50:28 -0400

    initial changes

---
+++ src/StartingOut/LearningDeveloperTest/BasicTests.cs
@@ -0,0 +1,29 @@
+﻿namespace LearningDeveloperTest;
+public class BasicTests
+{
+    [Fact]
+    public void CanAddTenAndTwenty()
+    {
+        // Given (Arrange) - this should recreate the world from scratch
+        int a = 10, b = 20;
+        int c;
+
+        //The important part - what we are actually testing.
+        //WHEN (Act)
+        c = a + b; //System Under Test (SUT)
+
+        //Then (Assert)
+        Assert.Equal(30, c);
+    }
+
+    [Theory]
+    [InlineData(10, 20, 30)]
+    [InlineData(4, 2, 6)]
+    public void CanAddAnyTwoIntegers(int a, int b, int expected)
+    {
+        int answer = a + b;
+
+        Assert.Equal(expected, answer);
+
+    }
+}
