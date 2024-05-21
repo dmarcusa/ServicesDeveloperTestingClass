3f9300431011b5e8c3783fef11f96df851d0122f
Dana Marcusanu <dmarcusanu@gmail.com>
2024-05-21 15:36:07 -0400

    drm

--- src/ReferenceSolution/ReferenceApi.ContractTests/HostFixture.cs
+++ src/ReferenceSolution/ReferenceApi.ContractTests/HostFixture.cs
@@ -1,16 +1,26 @@
 ﻿
 using Alba;
 using Microsoft.AspNetCore.TestHost;
+using Testcontainers.PostgreSql;
 
 namespace ReferenceApi.ContractTests;
 public class HostFixture : IAsyncLifetime
 {
     public IAlbaHost Host = null!;
 
+    private PostgreSqlContainer _container =
+        new PostgreSqlBuilder()
+        .WithImage("postgres:16.2-bullseye")
+        .WithUsername("testuser")
+        .WithPassword("tacosalad")
+        .WithDatabase("reference")
+        .Build();
+
     public async Task InitializeAsync()
     {
         Host = await AlbaHost.For<Program>(config =>
         {
+            config.UseSetting("ConnectionString:data", _container.GetConnectionString());
             config.ConfigureTestServices(services =>
             {
                 //var testFakeButtress = Substitute.For<ICheckForUniqueEmployeeStubs>();
@@ -21,6 +31,7 @@ public class HostFixture : IAsyncLifetime
     }
     public async Task DisposeAsync()
     {
+        await _container.StopAsync();
         await Host.DisposeAsync();
     }
 
